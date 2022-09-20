ae28f466f54286d9d3675060b9b1dce712d5d5f1
Joel André <joandre@student.ethz.ch>
2022-09-21 00:07:51 +0200

    fix

--- PyTorch/Detection/SSD/experiments/tpu_root_r1.sh
+++ PyTorch/Detection/SSD/experiments/tpu_root_r1.sh
@@ -5,4 +5,4 @@ export COCO_DIR=~/COCO
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
 export CHECKPOINT_DIR="./checkpoints"
 export SUMMARY_PATH=$CHECKPOINT_DIR/summary.json
-python3 main.py --trace traces/trace_gpu_coco.csv --rule root --seed 42 --backbone resnet50 --warmup 1000 --data $COCO_DIR --no-cuda --log-interval=10 --num_cores=1 --num-workers=8 --save $CHECKPOINT_DIR --json-summary $SUMMARY_PATH --parallel_loader 
+python3 main.py --trace traces/trace_tpu_coco.csv --rule root --seed 42 --backbone resnet50 --warmup 1000 --data $COCO_DIR --no-cuda --log-interval=10 --num_cores=1 --num-workers=8 --save $CHECKPOINT_DIR --json-summary $SUMMARY_PATH --parallel_loader 
