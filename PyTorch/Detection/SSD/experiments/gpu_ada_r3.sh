e9d7f0687f2fa58815027759fa26ec830450c3dd
Joel André <joandre@student.ethz.ch>
2022-09-21 18:11:43 +0200

    experiments

---
+++ PyTorch/Detection/SSD/experiments/gpu_ada_r3.sh
@@ -0,0 +1,8 @@
+# This script launches SSD300 training on a single TPUv3-8 core.
+# To launch: bash examples/SSD300_1CORE_BS128.sh
+
+export COCO_DIR=~/COCO
+export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+export CHECKPOINT_DIR="./checkpoints"
+export SUMMARY_PATH=$CHECKPOINT_DIR/summary.json
+python3 main.py --trace traces/trace_gpu_coco.csv --rule adascale --seed 44 --backbone resnet50 --warmup 1000 --data $COCO_DIR --no-cuda --log-interval=2 --num_cores=1 --num-workers=8 --save $CHECKPOINT_DIR --json-summary $SUMMARY_PATH --parallel_loader --epochs 95
